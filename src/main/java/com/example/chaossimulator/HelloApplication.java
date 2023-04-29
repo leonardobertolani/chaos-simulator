221232ac7bd24526ca9f057f329145a39abc1269
leuizz <297881@studenti.unimore.it>
2023-04-29 23:30:02 +0200

    Another problem

--- src/main/java/com/example/chaossimulator/HelloApplication.java
+++ src/main/java/com/example/chaossimulator/HelloApplication.java
@@ -7,6 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/*
+Another problem
+ */
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
